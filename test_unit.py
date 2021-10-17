4672503e1bf76d13b755cb45d4a0fbecbf947fef
snparab1704 <89501363+snparab1704@users.noreply.github.com>
2021-10-16 20:26:25 -0400

    Update test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 29 17:07:37 2021
-
-@author: deekay
-"""
 
 import pytest
 #from code.code import start_and_menu_command
 
-def test_start_and_menu_command_func():
+def sample_test():
     #test_result = start_and_menu_command("/start")
     #assert True == test_result, "Normal Case"
     print("Hello")
