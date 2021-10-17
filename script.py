9df5860f8e09640b3086a63f1fc9671ec0635c83
Sayali Parab <sayaliparab0494@gmail.com>
2021-10-16 21:09:18 -0400

    Adding structure

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
-print("Good Day, World!")
+""" script.py """
+
+def add(num1: int,num2: int):
+    """
+        Add 2 numbers
+    """
+    print("Good Day, World!")
+    return num1+num2
+
+if __name__ == "__main__":
+    RESULT = add(4,6)
+    print(RESULT)
--- test_unit.py
+++ test_unit.py
@@ -1,5 +1,23 @@
+""" test_unit.py """
 
-import pytest
+import unittest
 
-def sample_test():
-    print("Hello")
+from script import add
+
+
+class TestAdd(unittest.TestCase):
+    """ Test class """
+    def setUp(self) -> None:
+        pass
+
+    def test_add(self):
+        """
+            Test for Adding 2 numbers
+        """
+        num1 = 8
+        num2 = 2
+        result = add(num1, num2)
+        self.assertEqual(result, 10)
+
+if __name__ == "__main__":
+    unittest.main()
