9642bf1abb94acc891d3a67d91e076e359b6eb44
snparab1704 <89501363+snparab1704@users.noreply.github.com>
2021-10-16 20:26:03 -0400

    Create test_unit.py

---
+++ test_unit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 17:07:37 2021
+
+@author: deekay
+"""
+
+import pytest
+#from code.code import start_and_menu_command
+
+def test_start_and_menu_command_func():
+    #test_result = start_and_menu_command("/start")
+    #assert True == test_result, "Normal Case"
+    print("Hello")
