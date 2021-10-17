86636e9d9292f5e2ed4862e1a4335670e0e953c9
snparab1704 <89501363+snparab1704@users.noreply.github.com>
2021-10-16 20:26:48 -0400

    Update test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -1,8 +1,5 @@
 
 import pytest
-#from code.code import start_and_menu_command
 
 def sample_test():
-    #test_result = start_and_menu_command("/start")
-    #assert True == test_result, "Normal Case"
     print("Hello")
